7d38c02751b1010a91e645f5cea6cab050c458ab
martin-ngigi <martinwainaina001@gmail.com>
2022-11-23 10:21:01 +0300

    Changed data -> user

--- project/accounts/views.py
+++ project/accounts/views.py
@@ -32,7 +32,7 @@ class SignUpView(generics.GenericAPIView):
         
         if serializer.is_valid():
             serializer.save()
-            response = {"message": "User Created Successfully", "data": serializer.data}
+            response = {"message": "User Created Successfully", "user": serializer.data}
             # response = serializer.data
             return Response(data=response, status=status.HTTP_201_CREATED)
         #else
